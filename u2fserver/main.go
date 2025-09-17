70373e2cbdf222eebe83d4ffecec54017d734909
briskt <3172830+briskt@users.noreply.github.com>
2025-09-17 21:19:30 +0800

    simplify mux in u2fserver

--- u2fserver/main.go
+++ u2fserver/main.go
@@ -19,29 +19,9 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-// route is used to pass information about a particular route.
-type route struct {
-	Pattern     string
-	HandlerFunc http.HandlerFunc
-}
-
-// Define our routes.
-var routes = []route{
-	//   For information on this, see the doc comment for u2fsimulator.U2fRegistration
-	{
-		"POST /u2f/registration",
-		u2fsim.U2fRegistration,
-	},
-}
-
 // newRouter forms a new http.ServeMux
 func newRouter() *http.ServeMux {
 	mux := http.NewServeMux()
-
-	// Assign the handlers to run when endpoints are called.
-	for _, r := range routes {
-		mux.HandleFunc(r.Pattern, r.HandlerFunc)
-	}
-
+	mux.HandleFunc("POST /u2f/registration", u2fsim.U2fRegistration)
 	return mux
 }
