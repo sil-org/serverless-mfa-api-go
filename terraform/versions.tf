d6aef7941b27a5e946c0688b2ad06b005da16d49
briskt <3172830+briskt@users.noreply.github.com>
2026-07-08 08:23:21 -0600

    upgrade to Terraform 1.8 to support cross-resource moves

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,9 @@
 
 terraform {
-  required_version = ">= 1.0"
+  # Terraform 1.8 is required for cross-resource moving of resources, which is used in this module to move the
+  # Cloudflare DNS records from the old resource name (cloudflare_record) to the new resource name
+  # (cloudflare_dns_record).
+  required_version = ">= 1.8"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
